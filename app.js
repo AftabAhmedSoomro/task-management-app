025ee480cc33d91e7a038537707fe1504b22f355
Aftab Ahmed Soomro <154562779+AftabAhmedSoomro@users.noreply.github.com>
2024-04-04 10:31:17 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -48,3 +48,21 @@ function addDetail() {
         userDetail.value = ""
     }
 }
+
+function addBoth() {
+    let userText = document.querySelector("#task-input");
+    let userDetail = document.querySelector("#task-detail");
+
+    if (userText.value == "" || userDetail.value == "") {
+        alert("Please enter both task title and details!");
+    }
+    else {
+    let task = document.querySelector(".card-body");
+    task.innerHTML += ` <h3 class = "ms-3"><span><i class="fa-regular fa-square-check text-warning"></i></span> ${userText.value} </h3>`;
+    userText.value = "";
+
+    let taskDetail  = document.querySelector(".card-body");
+        taskDetail.innerHTML += ` <h6 class = "ms-5">${userDetail.value}</h6>`
+        userDetail.value = "";
+    }
+}
